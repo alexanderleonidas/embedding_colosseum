cce5799e0487059e4e63de38e160a169376d3745
Alexander Leonidas <alexanderleonidas@me.com>
2025-11-21 17:24:00 +0100

    Add brain tumor dataset support and update DataManager for new dataset integration

--- src/dataset/DataManager.py
+++ src/dataset/DataManager.py
@@ -1,7 +1,10 @@
 import torch
+
 from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
+
+from brain_tumor import extract_brain_tumor_dataset, BRAINTUMOR
 
 
 class DataManager:
@@ -9,78 +12,76 @@ class DataManager:
     Data Manager for loading a specified dataset.
     """
 
-    def __init__( self, batch_size: int, seed: int, dataset="mnist", pixel_size=None):
+    def __init__( self, batch_size: int, seed: int, pixel_size: int, dataset="mnist"):
         self.batch_size = batch_size
         self.generator = torch.Generator().manual_seed(seed)
-        if pixel_size is None:
-            transform = transforms.ToTensor()
-        else:
-            transform = transforms.Compose([transforms.ToTensor(), transforms.Resize((pixel_size, pixel_size))])
-        self.transform = transform
-
-        self.dataset_name = dataset
-        self.full_train, self.test_ds = self._get_dataset(dataset)
+        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Resize((pixel_size, pixel_size))])
+        self._data = self._get_dataset(dataset)
 
     def _get_dataset(self, dataset):
         if dataset == "mnist":
             full_train = datasets.MNIST(root="./data", train=True, download=True, transform=self.transform)
             test_ds = datasets.MNIST(root="./data", train=False, download=True, transform=self.transform)
+            all_data = torch.cat((full_train.data, test_ds.data), dim=0)
         elif dataset == "fashion":
             full_train = datasets.FashionMNIST(root="./data", train=True, download=True, transform=self.transform)
             test_ds = datasets.FashionMNIST(root="./data", train=False, download=True, transform=self.transform)
+            all_data = torch.cat((full_train.data, test_ds.data), dim=0)
         elif dataset == "cifar10":
             full_train = datasets.CIFAR10(root="./data", train=True, download=True, transform=self.transform)
             test_ds = datasets.CIFAR10(root="./data",train=False, download=True, transform=self.transform)
+            all_data = torch.cat((full_train.data, test_ds.data), dim=0)
         elif dataset == "stl10":
             full_train = datasets.STL10(root="./data", split="train", download=True, transform=self.transform)
             test_ds = datasets.STL10(root="./data", split="test", download=True, transform=self.transform)
+            all_data = torch.cat((full_train.data, test_ds.data), dim=0)
         elif dataset == "cxr8":
             raise NotImplemented("Dataset not supported yet.")
         elif dataset == "brain_tumor":
-            raise NotImplemented("Dataset not supported yet.")
-        elif dataset == "roco":
+            img_paths, labels = extract_brain_tumor_dataset()
+            all_data = BRAINTUMOR(img_paths, labels, transform=self.transform)
+        elif dataset == "eurosat":
             raise NotImplemented("Dataset not supported yet.")
         else:
-            raise ValueError("Unsupported dataset. Choose 'mnist', 'fashion', 'cifar10', 'stl10', 'cxr8', 'brain_tumor' or 'roco'.")
+            raise ValueError("Unsupported dataset. Choose 'mnist', 'fashion', 'cifar10', 'stl10', 'cxr8', 'brain_tumor_{ct, mri}' or 'eurosat'.")
 
-        return full_train, test_ds
+        return all_data
 
-    def get_loaders(self, val_split=None, train_size=None):
+    def get_loaders(self, train_split: float, val_split: float, test_split: float):
         """
-        Prepare DataLoaders for training and testing datasets. If val_split is specified, it splits the training data.
+        Prepare DataLoaders for training and testing datasets.
 
-        :return: Tuple of (train_loader, val_loader, test_loader) if val_split is specified, otherwise (train_loader, None, test_loader).
+        :param train_split: Proportion of training data.
+        :type train_split: float
+        :param val_split: Proportion of validation data.
+        :type val_split: float
+        :param test_split: Proportion of test data.
+        :type test_split: float
+        :return: Tuple of (train_loader, val_loader, test_loader)
         """
-        if train_size is not None:
-            if isinstance(train_size, float) and 0 < train_size <= 1.0:
-                train_size = int(len(self.full_train) * train_size)
-            else:
-                raise ValueError("train_size must be a float between 0 and 1 or a positive integer.")
-            self.full_train, _ = random_split(self.full_train, [train_size, len(self.full_train) - train_size], generator=self.generator)
+        if val_split <= 0 or val_split >= 1 or train_split <= 0 or train_split >= 1 or test_split <= 0 or test_split >= 1:
+            raise ValueError("Invalid split values. Values must be between 0 and 1.")
+        if val_split + train_split + test_split != 1:
+            raise ValueError("Sum of split values must be equal to 1.")
 
-        if val_split is not None and val_split > 0:
-            val_size = int(len(self.full_train) * val_split)
-            train_size = len(self.full_train) - val_size
-            train_ds, val_ds = random_split(self.full_train, [train_size, val_size])
+        test_size = int(len(self._data) * test_split)
+        train_size = int(len(self._data) * train_split)
+        val_size = len(self._data) - train_size - test_size
+        train_ds, val_ds, test_ds = random_split(self._data, [train_size, val_size, test_size])
 
-            train_loader = DataLoader(train_ds, batch_size=self.batch_size, shuffle=True)
-            val_loader = DataLoader(val_ds, batch_size=self.batch_size, shuffle=False)
-            test_loader = DataLoader(self.test_ds, batch_size=self.batch_size, shuffle=False)
-            return train_loader, val_loader, test_loader
-        else:
-            train_loader = DataLoader(self.full_train, batch_size=self.batch_size, shuffle=True)
-            test_loader = DataLoader(self.test_ds, batch_size=self.batch_size, shuffle=False)
-            return train_loader, None, test_loader
+        train_loader = DataLoader(train_ds, batch_size=self.batch_size, shuffle=True)
+        val_loader = DataLoader(val_ds, batch_size=self.batch_size, shuffle=False)
+        test_loader = DataLoader(test_ds, batch_size=self.batch_size, shuffle=False)
+        return train_loader, val_loader, test_loader
 
 
+
+# Example usage
 if __name__ == "__main__":
-    Dataloader = DataManager(batch_size=100, seed=42, dataset="mnist", pixel_size=100)
-
-    # print example image and its size
-    tr, ts = Dataloader.get_loaders()
-    img, _ = tr.dataset[0]
-    figure = plt.figure(figsize=(20, 4))
-    plt.axis("off")
-    plt.imshow(img.squeeze(), cmap="gray")
-    plt.show()
+    Dataloader = DataManager(batch_size=100, seed=42, dataset="brain_tumor", pixel_size=512)
+    train, val, test = Dataloader.get_loaders(0.8,0.1,0.1)
+    print(f"Train loader length: {len(train)}, Val loader length: {len(val)}, Test loader length: {len(test)}")
+    img, label = train.dataset[0]
     print(img.shape)
+    img = transforms.ToPILImage()(img)
+    img.show()
---
+++ src/dataset/brain_tumor.py
@@ -0,0 +1,53 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class BRAINTUMOR(Dataset):
+    def __init__(self, img_paths, labels, transform):
+        self.img_path = img_paths
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.img_path[idx]).convert("RGB")
+        label = self.labels[idx]
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+def extract_brain_tumor_dataset():
+    root = "./data/BRAINTUMOR"
+    ct_directory = os.path.join(root, "Brain Tumor CT scan Images")
+    mri_directory = os.path.join(root, "Brain Tumor MRI Images")
+    classes = ['Healthy', 'Tumor']
+
+    img_paths = []
+    labels = []
+    class_to_label = {'Healthy': 0, 'Tumor': 1}
+    if os.path.isdir(ct_directory) and os.path.isdir(mri_directory):
+        for class_name in classes:
+            ct_class_path = os.path.join(ct_directory, class_name)
+            mri_class_path = os.path.join(mri_directory, class_name)
+            if os.path.isdir(ct_class_path) and os.path.isdir(mri_class_path):
+                ct_img_files = [f for f in os.listdir(ct_class_path) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+                mri_img_files = [f for f in os.listdir(mri_class_path) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+                for ct_img_name in ct_img_files:
+                    img_paths.append(os.path.join(ct_class_path, ct_img_name))
+                    labels.append(class_to_label[class_name])
+                for mri_img_name in mri_img_files:
+                    img_paths.append(os.path.join(mri_class_path, mri_img_name))
+                    labels.append(class_to_label[class_name])
+
+    print(f"\nTotal Images: {len(img_paths)}")
+    print(f"Tumor (yes): {labels.count(1)} images ({labels.count(1) / len(labels) * 100:.1f}%)")
+    print(f"No Tumor (no): {labels.count(0)} images ({labels.count(0) / len(labels) * 100:.1f}%)")
+    return img_paths, labels
+
+if __name__ == "__main__":
+    img_paths, labels = extract_brain_tumor_dataset()
+    print(img_paths[:5])
+    print(labels[0])
