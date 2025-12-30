62fedd4d111976ea0007b3acf97a58f9d2d5133c
Frederik Grüneberg <agent21@alpenjodel.de>
2025-12-30 14:31:53 +0100

    Revert saving location back to hydra original

--- src/utils/save_training_progress.py
+++ src/utils/save_training_progress.py
@@ -2,6 +2,7 @@ import os
 from copy import copy
 
 import torch
+from hydra.core.hydra_config import HydraConfig
 from omegaconf import OmegaConf
 
 
@@ -10,62 +11,50 @@ class TrainingLogger:
         self.run_id = run_id
         self.cfg = cfg
         self.experiment_dir = self._get_exp_dir()
-        try:
-            self.save_cfg()
-        except Exception:
-            pass
-
-    def save_cfg(self):
-        filename = self._get_filename('.yaml', 'config')
-        OmegaConf.save(self.cfg, filename)
-        print(f'Config saved to {filename}')
 
     # Procedure to save a trained model
     def save_model_weights_and_bias(self, weights, bias, epoch):
-        filename = self._get_filename('.pt', f'saved_models/model_epoch-{epoch}')
-        copy_dict = {'weights': copy(weights), 'bias': copy(bias)}
+        filename = self._get_filename(".pt", f"saved_models/model_epoch-{epoch}")
+        copy_dict = {"weights": copy(weights), "bias": copy(bias)}
         torch.save(copy_dict, filename)
-        print(f'Model dict saved to {filename}')
-
+        print(f"Model dict saved to {filename}")
 
     # Procedure to save training data
     def save_training_progression(self, metrics):
-        filename = self._get_filename('.csv', 'training_progression')
+        filename = self._get_filename(".csv", "training_progression")
         file_exists = os.path.exists(filename)
-        with open(filename, 'a') as f:
+        with open(filename, "a") as f:
             if not file_exists:
-                f.write('epoch,train_loss,train_acc,val_loss,val_acc\n')
-            f.write(f"{metrics['epoch']},{metrics['epoch_loss']},{metrics['epoch_acc']},{metrics['val_loss']},{metrics['val_acc']}\n")
-
+                f.write("epoch,train_loss,train_acc,val_loss,val_acc\n")
+            f.write(
+                f"{metrics['epoch']},{metrics['epoch_loss']},{metrics['epoch_acc']},{metrics['val_loss']},{metrics['val_acc']}\n"
+            )
 
     # Procedure to save test data
     def save_test_progress(self, test_loss, exp_num=None):
-        filename = self._get_filename('.csv', 'test_results')
+        filename = self._get_filename(".csv", "test_results")
         file_exists = os.path.exists(filename)
-        with open(filename, 'a') as f:
+        with open(filename, "a") as f:
             if exp_num is None:
                 if not file_exists:
-                    f.write('test_loss\n')
+                    f.write("test_loss\n")
                 f.write(f"{test_loss}\n")
             else:
                 if not file_exists:
-                    f.write('run,test_loss\n')
+                    f.write("run,test_loss\n")
                 f.write(f"{exp_num},{test_loss}\n")
 
-    def _get_filename(self, extension, addition=''):
-        if extension[0] != '.' or extension == '' or extension is None:
-            raise ValueError('Please provide a valid file extension with a leading dot.')
+    def _get_filename(self, extension, addition=""):
+        if extension[0] != "." or extension == "" or extension is None:
+            raise ValueError(
+                "Please provide a valid file extension with a leading dot."
+            )
         return os.path.join(self.experiment_dir, f"{addition}{extension}")
 
     # Function to generate a unique filename for a saved model
     def _get_exp_dir(self):
-        root = os.path.dirname(os.path.abspath(__file__)).split('/src')[0]
-        file_dir = os.path.join(root, 'results')
-        os.makedirs(file_dir, exist_ok=True)
-        exp_dir = os.path.join(file_dir, f"run_{self.run_id}")
-        os.makedirs(exp_dir, exist_ok=True)
-        os.makedirs(os.path.join(exp_dir, 'saved_models'), exist_ok=True)
-        return exp_dir
+        return HydraConfig.get().runtime.output_dir
+
 
 # Example usage
 # tl = TrainingLogger(122, 0)
